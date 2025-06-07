4ef317e7c8a2d577eaec813917077b10b4567aca
Ericky2022 <92605654+Ericky2022@users.noreply.github.com>
2025-06-07 20:34:34 -0300

    Improve verse search and focus

--- src/app/versiculo-modal/versiculo-modal.component.ts
+++ src/app/versiculo-modal/versiculo-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output, AfterViewInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { TextToSpeechService } from '../services/text-to-speech.service';
 
@@ -7,7 +7,7 @@ import { TextToSpeechService } from '../services/text-to-speech.service';
   templateUrl: './versiculo-modal.component.html',
   styleUrls: ['./versiculo-modal.component.scss'],
 })
-export class VersiculoModalComponent implements AfterViewInit {
+export class VersiculoModalComponent {
   @Input() versiculos: any[] = []; // Inicialize como um array vazio
   @Input() livro: string = ''; // Inicialize como string vazia
   @Input() capitulo: number = 0; // Inicialize como 0
@@ -16,12 +16,11 @@ export class VersiculoModalComponent implements AfterViewInit {
 
   constructor(private modalController: ModalController, private tts: TextToSpeechService) { }
 
-  ngAfterViewInit() {
+  ionViewDidEnter() {
     if (this.versiculoFocado) {
-      setTimeout(() => {
-        const el = document.getElementById(`versiculo-${this.versiculoFocado.verse}`);
-        el?.scrollIntoView({ behavior: 'smooth', block: 'center' });
-      }, 0);
+      const id = `versiculo-${this.capitulo}-${this.versiculoFocado.verse}`;
+      const el = document.getElementById(id);
+      el?.scrollIntoView({ behavior: 'smooth', block: 'center' });
     }
   }
 
