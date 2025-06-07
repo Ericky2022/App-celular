158ec58b9669efac0ed24ba3133f79ec273dfbc2
Ericky2022 <92605654+Ericky2022@users.noreply.github.com>
2025-06-06 23:32:51 -0300

    test: add HTTP spec for IA service

--- src/app/services/ia.service.spec.ts
+++ src/app/services/ia.service.spec.ts
@@ -1,16 +1,44 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { IaService } from './ia.service';
 
 describe('IaService', () => {
   let service: IaService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(IaService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should POST to /api/gerarReflexao with the given verse', () => {
+    const versiculo = 'Joao 3:16';
+    const mockResponse = { texto: 'reflexao gerada' };
+
+    service.gerarReflexao(versiculo).subscribe((res) => {
+      expect(res).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne((request) =>
+      request.url.includes('/api/gerarReflexao')
+    );
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual({ versiculo });
+    req.flush(mockResponse);
   });
 });
